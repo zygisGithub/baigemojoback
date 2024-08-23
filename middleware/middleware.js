362674dd66bbd8bfa3e088e690b7aeba982e8cd1
dzigi <zygisbuzas@gmail.com>
2024-08-23 13:21:17 +0300

    pridetas login middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,19 +3,28 @@ require('dotenv').config();
 
 const middleware = {
     loginAuth: (req, res, next) => {
-        const token = req.headers.authorization?.split(' ')[1]; // Extract token from Authorization header
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required' });
+        }
+
+        next()
+    },
+    userAuth: (req, res, next) => {
+        const token = req.headers.authorization?.split(' ')[1]
 
         if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
+            return res.status(401).json({ message: 'No token provided' })
         }
 
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if (err) {
-                return res.status(403).json({ message: 'Failed to authenticate token' });
+                return res.status(403).json({ message: 'Failed to authenticate token' })
             }
-            req.user = decoded; // Save decoded token data to req.user
-            next();
-        });
+            req.user = decoded
+            next()
+        })
     },
 };
 
