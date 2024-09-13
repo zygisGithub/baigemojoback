4f3fb6c3f97beb23219ec27366115a99604dcbba
dzigi <zygisbuzas@gmail.com>
2024-09-13 13:32:27 +0300

    asd

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const io = initializeSocket(server);
 const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'authorization']
+    allowedHeaders: '*',
 };
 
 app.use(cors(corsOptions));
